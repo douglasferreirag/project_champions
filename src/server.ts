aa7fe65bcb2e3708f1ec4075b85258c8c6b562dd
douglasferreirag <douglas.ferreirag@gmail.com>
2026-03-15 20:03:23 -0300

    Configurando o express

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
-import express, {Request, Response} from "express";
+import express, {json, Request, Response} from "express";
 
 const app = express();
 
+const port = process.env.PORT;
+
+
+app.use(json());
+
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello, World!");
+    res.status(201).json({player: "Halland"});
 });
 
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`🔥Server is running on port http://localhost: ${port}`);
+});
 
 
