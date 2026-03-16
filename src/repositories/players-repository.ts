def00d04ed09c184626f8dc5178cecace202cee1
douglasferreirag <douglas.ferreirag@gmail.com>
2026-03-16 20:43:04 -0300

    Get list all players

--- src/repositories/players-repository.ts
+++ src/repositories/players-repository.ts
@@ -3,10 +3,102 @@ import { PlayerModel } from "../models/player-model";
 
 
 const database: PlayerModel[] = [
-
-    { id: 1, name: "Mbappé" },
-    { id: 2, name: "Haaland" },
-
+  {
+    "id": 1,
+    "name": "Vinicius Junior",
+    "club": "Real Madrid",
+    "nationality": "Brazil",
+    "position": "Forward",
+    "statistics": {
+      "Overall": 92,
+      "Pace": 95,
+      "Shooting": 89,
+      "Passing": 84,
+      "Dribbling": 94,
+      "Defending": 35,
+      "Physicality": 80
+    }
+  },
+  {
+    "id": 2,
+    "name": "Erling Haaland",
+    "club": "Manchester City",
+    "nationality": "Norway",
+    "position": "Striker",
+    "statistics": {
+      "Overall": 93,
+      "Pace": 89,
+      "Shooting": 96,
+      "Passing": 78,
+      "Dribbling": 86,
+      "Defending": 45,
+      "Physicality": 93
+    }
+  },
+  {
+    "id": 3,
+    "name": "Jamal Musiala",
+    "club": "Bayern Munich",
+    "nationality": "Germany",
+    "position": "Midfielder",
+    "statistics": {
+      "Overall": 89,
+      "Pace": 88,
+      "Shooting": 85,
+      "Passing": 88,
+      "Dribbling": 92,
+      "Defending": 60,
+      "Physicality": 72
+    }
+  },
+  {
+    "id": 4,
+    "name": "Kylian Mbappe",
+    "club": "Paris Saint-Germain",
+    "nationality": "France",
+    "position": "Forward",
+    "statistics": {
+      "Overall": 94,
+      "Pace": 97,
+      "Shooting": 92,
+      "Passing": 83,
+      "Dribbling": 93,
+      "Defending": 36,
+      "Physicality": 78
+    }
+  },
+  {
+    "id": 5,
+    "name": "Pedri",
+    "club": "Barcelona",
+    "nationality": "Spain",
+    "position": "Midfielder",
+    "statistics": {
+      "Overall": 88,
+      "Pace": 80,
+      "Shooting": 79,
+      "Passing": 92,
+      "Dribbling": 90,
+      "Defending": 70,
+      "Physicality": 72
+    }
+  },
+  {
+    "id": 6,
+    "name": "Lautaro Martinez",
+    "club": "Inter Milan",
+    "nationality": "Argentina",
+    "position": "Forward",
+    "statistics": {
+      "Overall": 90,
+      "Pace": 85,
+      "Shooting": 91,
+      "Passing": 82,
+      "Dribbling": 87,
+      "Defending": 42,
+      "Physicality": 84
+    }
+  }
 ]
 
 export const findAllPlayers = async () : Promise<PlayerModel[]> => {
