b871825e04ae65dc486c06197dbe461630089d74
douglasferreirag <douglas.ferreirag@gmail.com>
2026-03-16 17:32:11 -0300

    Criando a camada de Utils

--- src/controllers/players-controller.ts
+++ src/controllers/players-controller.ts
@@ -1,11 +1,16 @@
 import { Request, Response } from "express";
 import { getPlayerService } from "../services/players/players-service";
+import { ok } from "../utilidades/http-helper";
 
 export const getPlayer = async (req: Request, res: Response) => {
     
     const data = await getPlayerService();
+
+    const httpResponse = await ok(data);
     
-    res.status(201).json(data);
+    res.status(httpResponse.statusCode).json(httpResponse.body);
+
+    
 
 
 }
---
+++ src/utilidades/http-helper.ts
@@ -0,0 +1,10 @@
+interface HttpResponse {
+    statusCode: number;
+    body: any;
+}   
+
+export const ok = async (data: any): Promise<HttpResponse> => ({
+    statusCode: 200,
+    body: data
+}); 
+
