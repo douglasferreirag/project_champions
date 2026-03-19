44b2fef8c6c4e35e48a17f58525800ed576daa3b
douglasferreirag <douglas.ferreirag@gmail.com>
2026-03-19 15:13:12 -0300

    Lendo arquivo Json

--- src/repositories/clubs-repository.ts
+++ src/repositories/clubs-repository.ts
@@ -1,4 +1,6 @@
 import { ClubModel } from "../models/club-model";
+import fs from "fs/promises";
+
 
 
  const database = [
@@ -15,8 +17,10 @@ import { ClubModel } from "../models/club-model";
 
 export const findAllClubs = async () : Promise<ClubModel[]> => {
 
+    const data = await fs.readFile("./src/data/clubs.json", "utf-8");
 
+    const clubs: ClubModel[] = JSON.parse(data);
 
-    return database;   
+    return clubs;   
     
 }
