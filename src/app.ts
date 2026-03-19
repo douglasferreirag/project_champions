24fcaa37a5d139134b165cf30d9d431921d34ca8
douglasferreirag <douglas.ferreirag@gmail.com>
2026-03-19 15:52:35 -0300

    Implementando o cors

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express from "express";
 
 import router from "./routes";
 
+import cors from 'cors';       
+import { METHODS } from "node:http";
+
 
 function createApp() {
 
@@ -10,6 +13,16 @@ function createApp() {
         app.use(express.json());
 
         app.use("/api", router);
+
+        //Delimitando quem pode usar a api no corsOption
+
+        const corsOptions = {
+
+                origin: ['http://douglas.system.com', "http://gov.br"],
+                METHODS : ['GET', "UPDATE" ]       
+        }
+
+        app.use();
 
         return app;
 
